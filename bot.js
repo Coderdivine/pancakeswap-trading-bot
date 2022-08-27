6c453e2773e574734b72edcd7a8a6c4a1218f666
Coderdivine <chimdi4332@gmail.com>
2022-08-27 23:45:41 +0100

    main

--- bot.js
+++ bot.js
@@ -285,7 +285,7 @@ async function makeSwap(balance,toBuyValue,toSellValue,rate,period) {
 }
 async function GetData() {
     const res = await botss.find()
-        if (res.length) {
+        if (res.length <= 0) {
             last = res[0].last;
             last_point = res[0].last_point;
             _string = res[0]._string;
